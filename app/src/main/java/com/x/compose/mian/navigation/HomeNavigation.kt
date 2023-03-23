2d30a7a1d8c4069f2e75db31f1ae7921df710a1c
xu <xu.guo@1step.ai>
2023-03-23 17:42:25 +0800

    feat:[AnimatedNavHost] 学习AnimatedNavHost 动画参数

--- app/src/main/java/com/x/compose/mian/navigation/HomeNavigation.kt
+++ app/src/main/java/com/x/compose/mian/navigation/HomeNavigation.kt
@@ -1,7 +1,18 @@
 package com.x.compose.mian.navigation
 
+import androidx.compose.animation.AnimatedContentScope
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
+import androidx.compose.animation.slideIn
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.unit.IntSize
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.navigation
@@ -19,7 +30,31 @@ fun HomeNavigate() {
     val navController = rememberAnimatedNavController()
     AnimatedNavHost(
         navController = navController,
-        startDestination = HOME_NAVIGATION
+        startDestination = HOME_NAVIGATION,
+        enterTransition = { EnterTransition.None },
+        exitTransition = { ExitTransition.None }
+//        enterTransition = {
+//            slideIntoContainer(
+//                AnimatedContentScope.SlideDirection.Left,
+//                animationSpec = tween(300)
+//            )
+//            scaleIn(
+//                animationSpec = tween(700),
+//                initialScale = 0.2f,
+//                transformOrigin = TransformOrigin(0.2f, 0.2f)
+//            )
+//        },
+//        exitTransition = {
+//            slideOutOfContainer(
+//                AnimatedContentScope.SlideDirection.Right,
+//                animationSpec = tween(300)
+//            )
+//            scaleOut(
+//                animationSpec = tween(700),
+//                targetScale = 0.2f,
+//                transformOrigin = TransformOrigin(0.2f, 0.2f)
+//            )
+//        }
     ) {
         navigation(startDestination = HOME_NAVIGATION_DEFAULT, route = HOME_NAVIGATION) {
             composable(
@@ -39,7 +74,7 @@ fun HomeNavigate() {
 fun NavGraphBuilder.baseGraph(navController: NavController) {
     navigation(startDestination = "a", route = "b") {
         composable(
-           route =  BASE_NAVIGATION_TEXT
+            route = BASE_NAVIGATION_TEXT
         ) {
             BaseTextPage {
                 navController.popBackStack()
